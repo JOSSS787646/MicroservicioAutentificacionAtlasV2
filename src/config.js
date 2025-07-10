d8ee7af020421e7d2f626aa2ea51a6d73fc43f39
JOSS787646 <joseantoni787646@gmail.com>
2025-07-10 10:20:26 -0600

    Actulizacion a atlas

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
-require('dotenv').config();
-
 module.exports = {
-  port: process.env.PORT || 4000,
-  mongodbUri: process.env.MONGO_URI,   // Aquí debe coincidir el nombre exacto de la variable
-  jwtSecret: process.env.JWT_SECRET,
+  port: 4000,
+  mongodbUri: 'mongodb+srv://joseantoni7876461203:7876460000_Jose@cluster0.ttxmmm1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  jwtSecret: 'clave_super_secreta_que_tu_definas_aqui',
 };
