d0ee3986d1835411860e9eac3342dfe000dec70b
JOSS787646 <joseantoni787646@gmail.com>
2025-07-10 10:28:38 -0600

    Actulizacion de atlas con acceso publico

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  port: 4000,
   mongodbUri: 'mongodb+srv://joseantoni7876461203:7876460000_Jose@cluster0.ttxmmm1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   jwtSecret: 'clave_super_secreta_que_tu_definas_aqui',
 };
